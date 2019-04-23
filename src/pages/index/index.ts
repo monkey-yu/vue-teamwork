42f99a3bf45ea884458c32ea6e1f5a35d58263c9
zhaoyu <914888156@qq.com>
2019-04-23 16:41:08 +0800

    task-添加 主页右侧导航

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -2,5 +2,10 @@ import Vue from 'vue'
 import Component from 'vue-class-component'
 @Component({})
 export default class Index extends Vue{
-
+  handleOpen(key, keyPath) {
+    console.log(key, keyPath);
+  }
+  handleClose(key, keyPath) {
+    console.log(key, keyPath);
+  }
 }
